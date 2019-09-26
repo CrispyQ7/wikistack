7c23353e4a6edf719a5290fb8b59b4574cadbd4c
Ian Roberts <irob466@gmail.com>
2019-09-26 14:57:59 -0500

    added the post route

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const { main, addPage, editPage } = require("../views");
-const { db } = require('../models');
+const { Page } = require('../models');
+
+const slugify = title => title.replace(/\s+/g, '_').replace(/\W/g, '');
 
 router.get("/", (req, res) => {
   res.send(main());
 })
 
-router.post("/", (req, res) => {
-  res.send("");
+router.post("/", async (req, res, next) => {
+  const page = new Page({
+    title: req.body.title,
+    content: req.body.content,
+  });
+
+  try {
+    await page.save();
+    res.redirect('/');
+  } catch (error) { next(error) }
 })
 
 router.get("/add", (req, res) => {
